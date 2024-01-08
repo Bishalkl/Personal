213acf24291fa25e55e491118c576b69cc00a82d
Bishal koirala <142210148+Bishalkl@users.noreply.github.com>
2024-01-08 14:13:45 +0545

    done

---
+++ C-language--main/loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    // initi
+    int x = 1;
+
+    // for loop
+    for(int i = 0; i<=10; i++){
+        printf("%d ",x);
+        x = x * 10  + 1;
+    }
+
+    return 0;
+}
+
+
