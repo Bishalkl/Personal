12bd63fb21e2869215b097dd545573865557b6ee
Bishal koirala <bishal123koirala@gmail.com>
2024-02-01 13:51:33 +0545

    done

---
+++ web dev/js course work/script.js
@@ -0,0 +1,12 @@
+// console.log(`Hello world`);
+// // window.alert(`This is an alert`);
+
+// // how to use it to html
+document.getElementById("myh1").textContent="Hello";
+document.getElementById("myp").textContent="my name is bishal koirala."
+
+let x = 12;
+let y = 12;
+console.log(`Value of x is ${x} and Value of y is ${y}.`);
+console.log(typeof x);
+console.log(typeof y);
